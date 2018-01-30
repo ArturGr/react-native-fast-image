7e180beaa2a3354817c03fad88956689de8ef9f8
ArturGr <graefenstein@petesso.de>
2018-01-30 20:16:20 +0200

    Update FastImage.js
    add rounded

--- FastImage.js
+++ FastImage.js
@@ -1,12 +1,10 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import {
-  View,
   Image,
   NativeModules,
   requireNativeComponent,
   ViewPropTypes,
-  StyleSheet,
 } from 'react-native'
 
 const resolveAssetSource = require('react-native/Libraries/Image/resolveAssetSource')
@@ -26,9 +24,6 @@ class FastImage extends Component {
       onLoad,
       onError,
       onLoadEnd,
-      style,
-      children,
-      borderRadius,
       ...props
     } = this.props
 
@@ -49,50 +44,20 @@ class FastImage extends Component {
     }
 
     const resolvedSource = resolveAssetSource(source)
-
-    if (!children && !borderRadius) {
-      return (
-        <FastImageView
-          ref={e => (this._root = e)}
-          {...props}
-          style={style}
-          source={resolvedSource}
-          onFastImageLoadStart={onLoadStart}
-          onFastImageProgress={onProgress}
-          onFastImageLoad={onLoad}
-          onFastImageError={onError}
-          onFastImageLoadEnd={onLoadEnd}
-        />
-      )
-    }
-
     return (
-      <View style={style} borderRadius={borderRadius}>
-        <View style={styles.imageContainer} borderRadius={borderRadius}>
-          <FastImageView
-            ref={e => (this._root = e)}
-            {...props}
-            style={StyleSheet.absoluteFill}
-            source={resolvedSource}
-            onFastImageLoadStart={onLoadStart}
-            onFastImageProgress={onProgress}
-            onFastImageLoad={onLoad}
-            onFastImageError={onError}
-            onFastImageLoadEnd={onLoadEnd}
-          />
-        </View>
-        {children}
-      </View>
+      <FastImageView
+        ref={e => (this._root = e)}
+        {...props}
+        source={resolvedSource}
+        onFastImageLoadStart={onLoadStart}
+        onFastImageProgress={onProgress}
+        onFastImageLoad={onLoad}
+        onFastImageError={onError}
+        onFastImageLoadEnd={onLoadEnd}
+      />
     )
   }
 }
-
-const styles = StyleSheet.create({
-  imageContainer: {
-    ...StyleSheet.absoluteFillObject,
-    overflow: 'hidden',
-  },
-})
 
 FastImage.resizeMode = {
   contain: 'contain',
@@ -119,6 +84,7 @@ const FastImageSourcePropType = PropTypes.shape({
   uri: PropTypes.string,
   headers: PropTypes.objectOf(PropTypes.string),
   priority: PropTypes.oneOf(Object.keys(FastImage.priority)),
+  rounded: PropTypes.bool,
 })
 
 FastImage.propTypes = {
